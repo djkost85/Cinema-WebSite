70d9ccde56aba3e0b4d4e2abd64b74124c008a72
jerome <jeromebarotin@gmail.com>
2011-01-09 17:16:07 +0100

    Initiation fichier de config

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+/***************************************
+ * Fichier de config de l'application
+ ***************************************/
+
+/*----------
+ * Allocine
+ -----------*/
+
+/// Code allociné associé au cinéma
+define("ALLOCINE_CINEMA_CODE", "W0419");
+
+/// Code postal du cinéma
+define("ALLOCINE_CINEMA_POSTCODE", "35130")
+
+
+
+/// Nombre de jour après la selection de la date sur laquelle on va requêter le site allocine
+define("ALLOCINE_NB_DAY_REQUEST",30);
+
+/*----------
+ * Session
+ -----------*/
+
+/// Clé de session sous laquelle seront enregistré tout les 
+define("SESSION_KEY", "LE_BRETAGNE_NEWSLETTER");
+
+
+?>

319fdc214a99f6394758a49e2520192f24a1d6e5
jerome <jeromebarotin@gmail.com>
2011-01-09 20:57:04 +0100

    Ajout détail sur le modèle de BD Film

--- class/model/Film.php
+++ class/model/Film.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once("class/model/Prog.php");
+require_once("class/sql/DbField.php");
+require_once("class/model/Projection.php");
 
 /**
  * 
@@ -8,10 +9,7 @@ require_once("class/model/Prog.php");
  * de la table Film
  *
  */
-class Film {
-	
-	/// id unique du champ dans la base
-	private $id;
+class Film extends DbField {
 
 	/// Titre du film
 	private $title;
@@ -34,14 +32,14 @@ class Film {
 	/// Les contenus sont "VF" ou "VOST"
 	private $soundVersion;
 	
-	/// Age minimum pour voir le film 
-	private $oldForbade;
+	/// Message d'avertissement associé au film 
+	private $avertissementMsg;
 	
 	/// Id référence Allociné
 	private $allocineId;
 	
-	/// Liste des programations du film
-	private $prog;
+	/// Liste des projections programmée du film
+	private $projection;
 	
 	/**
 	 * 
@@ -56,39 +54,115 @@ class Film {
 	 * @param $id
 	 */
 	function __construct($title, $originalTitle ="", $resume = "", $poster ="", $year = 0, $allocineId = 0, $id = 0 ){
+		parent::__construct("Film",$id);
+		
 		$this->title = $title;
 		$this->originalTitle = $originalTitle;
 		$this->resume = $resume;
 		$this->poster = $poster;
 		$this->year = $year;
-		$this->allocineId = $allocineId;
-		$this->id = $id;		
-		$this->prog = array();
+		$this->allocineId = $allocineId;		
+		$this->projection = array();	
 		
 	}	
 	
-	function addProg(Prog $prog){
-		$this->prog[] = $prog;
+	function addProjection(Projection $projection){
+		$this->projection[] = $projection;
 	}
 	
+	function getProjection(){
+		return $this->projection;	
+	}
 	/*GETTER*/
-	
-	function getId(){
-		return $this->id;
-	}
 	
 	function getTitle(){
 		return $this->title;
+	}
+	
+	function getOriginalTitle(){
+		return $this->originalTitle;
+	}
+	
+	function setOriginalTitle($originalTitle){
+		$this->originalTitle = $originalTitle;
+	}
+	
+	function getNationality(){
+		return $this->nationality;
+	}
+	
+	function setNationality($nationality){
+		$this->nationality = $nationality;
 	}
 	
 	function getResume(){
 		return $this->resume;
 	}
 	
+	function setResume($resume){
+		$this->resume = $resume;
+	}
+	
+	function getYear() {
+		return $this->year;
+	}
+	
+	function setYear($year){
+		$this->year = $year;
+	}
+	
 	function getPoster(){
 		return $this->poster;
 	}
-	 
+	
+	function setPoster($poster){
+		$this->poster = $poster;
+	}
+	
+	function getAvertissementMsg(){
+		return $this->avertissementMsg;
+	}
+	
+	function setAvertissementMsg($avertissementMsg){
+		$this->avertissementMsg = $avertissementMsg; 
+	}
+	
+	function getAllocineId(){
+		return $this->allocineId;
+	}
+	
+	function setAllocineId($allocineId){
+		$this->allocineId = $allocineId;
+	}
+	
+	
+	/**
+	 * Stocke les nouvelles données dans la base de donnée
+	 */
+	function storeInDb(){
+		
+		//On stocke le film
+		$fields = array(	$this->title => new DbEntry("title", "string"), 
+							$this->originalTitle => new DbEntry("originalTitle","string"),
+							$this->nationality => new DbEntry("nationality", "string"),
+							$this->resume => new DbEntry("resume", "string"),
+							$this->poster => new DbEntry("poster", "string"),
+							$this->year => new DbEntry("year", "int"),
+							$this->soundVersion => new DbEntry("soundVersion", "enum"),
+							$this->avertissementMsg => new DbEntry("avertissementMsg", "string"),
+							$this->allocineId => new DbEntry("allocineId", "string")
+			);
+	
+		parent::makeStoreRequest($fields);
+
+		//On stocke les projections en base
+		foreach($this->projection as $projection){
+			$projection->storeInDb();
+		}
+		
+				
+	}
+	
 	/**
 	 * 
 	 * Fonction utilisé par array_unique

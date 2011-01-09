d57b4af1aefa10f7ca11c0f16376798071ef89f8
jerome <jeromebarotin@gmail.com>
2011-01-09 11:51:20 +0100

    Ajout fichier application exécutant le site web

---
+++ class/kernel/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('lib/ets/ets.php');
+
+/**
+ * 
+ * Point d'entrée du site 
+ * du cinémas
+ * 
+ */
+class Application {
+	
+	private $tplPath;
+	
+	function __construct($tplPath){
+		$this->tplPath = $tplPath;
+	}
+	
+	function run(){
+		
+	}
+	
+}
+
+?>

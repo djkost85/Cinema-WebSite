443e94a7d9aacce9badc74aa70f9ff6c5ed255e9
jerome <jeromebarotin@gmail.com>
2011-01-09 11:51:02 +0100

    Ajout champ dans table film

--- class/model/Film.php
+++ class/model/Film.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once("class/model/Prog.php");
 
 /**
  * 
@@ -15,15 +16,60 @@ class Film {
 	/// Titre du film
 	private $title;
 	
+	/// Titre original du film
+	private $originalTitle;
+	
+	/// Nationalité du film
+	private $nationality;
+	
 	/// Résumé du film
 	private $resume;
 	
 	/// Chemin vers l'image correspondant à l'affiche du film
-	private $poster;
+	private $poster; 
 	
-	function __construct(){
+	/// Année du film
+	private $year;
+		
+	/// Les contenus sont "VF" ou "VOST"
+	private $soundVersion;
+	
+	/// Age minimum pour voir le film 
+	private $oldForbade;
+	
+	/// Id référence Allociné
+	private $allocineId;
+	
+	/// Liste des programations du film
+	private $prog;
+	
+	/**
+	 * 
+	 * Constructeur
+	 *  
+	 * @param $title
+	 * @param $originalTitle
+	 * @param $resume
+	 * @param $poster
+	 * @param $year
+	 * @param $allocineId
+	 * @param $id
+	 */
+	function __construct($title, $originalTitle ="", $resume = "", $poster ="", $year = 0, $allocineId = 0, $id = 0 ){
+		$this->title = $title;
+		$this->originalTitle = $originalTitle;
+		$this->resume = $resume;
+		$this->poster = $poster;
+		$this->year = $year;
+		$this->allocineId = $allocineId;
+		$this->id = $id;		
+		$this->prog = array();
 		
 	}	
+	
+	function addProg(Prog $prog){
+		$this->prog[] = $prog;
+	}
 	
 	/*GETTER*/
 	
@@ -42,7 +88,14 @@ class Film {
 	function getPoster(){
 		return $this->poster;
 	}
-	
+	 
+	/**
+	 * 
+	 * Fonction utilisé par array_unique
+	 */
+ 	function __toString(){
+		return $this->title;
+	}
 	
 	
 }
